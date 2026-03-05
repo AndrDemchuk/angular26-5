1f2c34d4efc5a3bc405d157c1d8862f70fca4f20
AndrDemchuk <xdemchuk@gmail.com>
2026-03-05 15:44:10 +0200

    environments specified; angular material cards added

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../../models/product/product';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -6,4 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {}
+export class ProductListComponent implements OnInit {
+
+  public products$!: Observable<Product[]>;
+  constructor(private productService: ProductService, private cd: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts()
+  }
+
+}
